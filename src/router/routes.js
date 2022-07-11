62fd090b7a9b3617b521b96b80895bf27a599873
bragaus <sprtj@protonmail.com>
2022-07-11 06:20:19 -0300

    algumas modificações de ontem

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,7 +8,7 @@ const routes = [
     ]
   },
   {
-    path: '/',
+    path: '/auth',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
